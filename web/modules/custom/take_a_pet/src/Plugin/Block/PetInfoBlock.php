9f804516a5692e3f04dccafbe5c8d1aa694907c7
Alvaro Padilla <alvaro.padilla@bitsamericas.com>
2021-08-01 21:41:45 -0500

    Definiendo bloque personalizado

--- web/modules/custom/take_a_pet/src/Plugin/Block/PetInfoBlock.php
+++ web/modules/custom/take_a_pet/src/Plugin/Block/PetInfoBlock.php
@@ -1,7 +1,31 @@
 <?php
 
+namespace Drupal\take_a_pet\Plugin\Block;
 
-class PetInfoBlock
-{
+/**
+ * Provides a pet info block
+ * @Block (
+ *   id = "pet_info_block",
+ *   admin_label = @Translation("Pet info Block")
+ * )
+ */
 
+use Drupal\Core\Block\BlockBase;
+
+class PetInfoBlock extends BlockBase{
+
+
+  public function build()
+  {
+    $path = "https://pixabay.com/photos/2018/04/27/19/11/anonumous-3355586_960_728.png";
+    return [
+        '#markup' => "<div id='pet_info'>
+                        <img id='pet_image' src='$path'>
+                        <h1 id='pet_name'>Select a pet</h1>
+                        <h2 id='pet_age'></h2>
+                        <h2 id='foundation_name'></h2>
+                        <h2 id='foundation_email'></h2>
+                      <div>"
+    ];
+  }
 }
